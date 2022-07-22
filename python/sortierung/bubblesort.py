2ace25dd2f4fc30e74b71bbb962611560e6133d9
Mika Reich <mikareich0@gmail.com>
2022-07-22 08:09:29 +0000

    did a lot of stuff

--- python/sortierung/bubblesort.py
+++ python/sortierung/bubblesort.py
@@ -1,10 +1,9 @@
 import random
-
 from timer import Timer
 
 timer = Timer()
 
-timer.startTimer()
+
 
 def bubblesort(liste):
     for i in range(len(liste)):
@@ -16,6 +15,6 @@ def bubblesort(liste):
 def generiereListe(laenge):
     return [random.randint(0, laenge) for i in range(laenge)]
 
-print('100 Elemente:' + str(bubblesort(generiereListe(100))))
-
-print(timer.endTimer())
+timer.startTimer()
+print('Elemente: \033[93m 10000 \033[92m\n' + str(bubblesort(generiereListe(10000))))
+print('\033[0m Passed time: \033[93m', timer.endTimer())
--- python/sortierung/minsort.py
+++ python/sortierung/minsort.py
@@ -1,4 +1,8 @@
 import random
+from timer import Timer
+
+timer = Timer()
+
 
 def minsort(list):
     if len(list) <= 1:
@@ -12,10 +16,6 @@ def minsort(list):
 def generate_list(length):
     return [random.randint(0, length) for i in range(length)]
 
-print('100 Elemente:' + str(minsort(generate_list(100))))
-print('==============================')
-print('1000 Elemente:' + str(minsort(generate_list(1000))))
-print('==============================')
-print('10000 Elemente:' + str(minsort(generate_list(10000))))
-print('==============================')
-print('100000 Elemente:' + str(minsort(generate_list(100000))))
+timer.startTimer()
+print('100000 Elemente:' + str(minsort(generate_list(10000))))
+print(timer.endTimer())
