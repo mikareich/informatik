9e4d39533cc0ac0c13e694389dad4196c2f0dce1
Mika Reich <mikareich0@gmail.com>
2023-09-06 15:51:55 +0200

    stuff

--- python/kommunikation_und_rechennetzwerke/rts_cts.py
+++ python/kommunikation_und_rechennetzwerke/rts_cts.py
@@ -33,32 +33,32 @@ class App(Tk):
 
         # Window settings
         self.title("RTS und CTS")
-        self.geometry('300x100+100+200')
+        self.geometry("300x100+100+200")
 
         # RTS Button
-        button = Button(master=self, text="RTS")
-        button.bind('<Button-1>', self.serial.rtsOn)
-        button.bind('<ButtonRelease>', self.serial.rtsOff)
+        button = Button(master=self, text="RTS an/aus")
+        button.bind("<Button-1>", self.serial.rtsOn)
+        button.bind("<ButtonRelease>", self.serial.rtsOff)
         button.place(x=70, y=35)
 
         # CTS Led
         self.canvas = Canvas(master=self, width=60, height=60)
-        self.canvas.place(x=100, y=20)
-        item = self.canvas.create_oval(10, 10, 50, 50, fill='#550000')
-        self.canvas.itemconfig(item, tags=('LED'))
+        self.canvas.place(x=170, y=20)
+        item = self.canvas.create_oval(10, 10, 50, 50, fill="#550000")
+        self.canvas.itemconfig(item, tags=("LED"))
 
         # Call checkCts and start mainloop
-        self.mainloop()
         self.checkCts()
+        self.mainloop()
 
     def checkCts(self):
         if self.serial.getCts():
-            self.canvas.itemconfig('LED', fill='#ff1200')
+            self.canvas.itemconfig("LED", fill="#ff0000")
         else:
-            self.canvas.itemconfig('LED', fill='#550000')
+            self.canvas.itemconfig("LED", fill="#550000")
 
         self.after(100, self.checkCts)
 
 
-serial = Serial('COM3')
+serial = Serial("COM3")
 view = App(serial)
